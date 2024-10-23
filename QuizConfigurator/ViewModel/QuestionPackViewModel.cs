d8706ad66d5a4060adb8e4c78f9c1418076d0b72
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-10-23 11:56:34 +0200

    Add Model and ViewModel

---
+++ QuizConfigurator/ViewModel/QuestionPackViewModel.cs
@@ -0,0 +1,46 @@
+﻿using QuizConfigurator.Model;
+using System.Collections.ObjectModel;
+
+namespace QuizConfigurator.ViewModel
+{
+    class QuestionPackViewModel : ViewModelBase
+    {
+        public QuestionPackViewModel(QuestionPack model)
+        {
+            this.model = model;
+            this.Questions = new ObservableCollection<Question>(model.Questions);
+        }
+        private readonly QuestionPack model;
+
+
+        public string Name 
+        { 
+            get => model.Name;
+            set 
+            {
+                model.Name = value;
+                RaisePropertyChanged();
+            }
+        }
+        public Difficulty Difficulty
+        {
+            get => model.Difficulty;
+            set
+            {
+                model.Difficulty = value;
+                RaisePropertyChanged();
+            }
+        }
+        public int TimeLimitInSeconds
+        {
+            get => model.TimeLimitInSeconds;
+            set
+            {
+                model.TimeLimitInSeconds = value;
+                RaisePropertyChanged();
+            }
+        }
+        public ObservableCollection<Question> Questions { get; }
+
+    }
+}
